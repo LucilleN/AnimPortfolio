1981c252b3b00a18faf2739388162026cd029dc1
LucilleN <33737001+LucilleN@users.noreply.github.com>
2018-09-19 00:06:35 -0700

    Create animportfolio.js

---
+++ animportfolio.js
@@ -0,0 +1,27 @@
+function getWidth() {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  );
+}
+
+function getHeight() {
+  return Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.documentElement.clientHeight
+  );
+}
+
+let objectWidth = 640;
+let objectHeight = 360;
+
+function scaleObject() {
+  objectWidth = getWidth();
+  objectHeight = objectWidth * 360/640;
+}
